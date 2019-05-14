160100388986926a64f09a33eb190e5047b995a7
daytona211 <znorcross@albany.edu>
2019-05-14 12:11:32 -0400

    breaking DB connection

--- server/app.js
+++ server/app.js
@@ -7,13 +7,13 @@ app.get("/", (req, res)=>{
     res.send("WORKS");
 })
 
-const db = new Client({
-  user: "jdcmfswfoasasa",
-  host: "ec2-54-163-226-238.compute-1.amazonaws.com",  
-  database: "d14n9n2ii7vbu5",
-  password: "0cc20c0a9be1623865e96b17726981adcfe03e4215fe40aadf8d76fd78fe4d90",
-  port: 5432
-});
+// const db = new Client({
+//   user: "jdcmfswfoasasa",
+//   host: "ec2-54-163-226-238.compute-1.amazonaws.com",  
+//   database: "d14n9n2ii7vbu5",
+//   password: "0cc20c0a9be1623865e96b17726981adcfe03e4215fe40aadf8d76fd78fe4d90",
+//   port: 5432
+// });
 module.exports.db = db;
 db.connect(() => {
   console.log("Connected to DB");
